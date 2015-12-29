8abc6e771c61677a84b071333965bf9311caaaff
vovhiklev1 <vovhiklev1@gmail.com>
2015-12-29 03:30:38 +0200

    верстка 55%

--- app/directives/mailContentDirective.js
+++ app/directives/mailContentDirective.js
@@ -17,7 +17,7 @@ app.directive('mailContentDirective', function (urlRouteFactory, $compile) {
         restrict: 'AE',
         controller: 'mailContentCtrl',
         link: function (scope, elem, attrs, ctrl, transclude) {
-            elem.replaceWith($compile('<div class="d" ' + scope.temp + ' >template2</div>')(scope));
+            elem.replaceWith($compile('<div class="row"><div class="col-md-12" ' + scope.temp + ' >template2</div></div>')(scope));
             console.log('mailContentDirective1 ' + scope.temp + ' hi ')
         }
 
